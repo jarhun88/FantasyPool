c704e3442fc022fa132e870ab6ba2db512cf588b
john <likisongproduct@gmail.com>
2018-05-06 02:17:13 -0700

    first commit

---
+++ linked_list.cpp
@@ -0,0 +1,40 @@
+#include "linked_list.h"
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+/**
+ * Linked List Constructor
+ */
+
+List::List(string name){
+    head = NULL;
+    owner = name;
+}
+
+/**
+ * Insert new Node at the front (head) of the linked list
+ * previous head is now at second of the linked list
+ */
+void List::insert(Node*& head, string newData){
+    Node * curr = new Node;
+    curr->data = newData;
+    curr->next = head;
+    head = curr;
+}
+
+void List::remove(Node*& head){
+    Node * temp = head;
+    head = temp->next;
+    temp->next = NULL;
+    delete temp;
+}
+// prints all values in linked list starting from head
+void List::print(Node* head){
+    Node * curr = head;
+    while (curr != NULL){
+        cout << curr->data << " " << endl;
+        curr = curr->next;
+    }
+}
